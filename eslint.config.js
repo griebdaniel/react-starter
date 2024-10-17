ded92a352aeee7fb8834600008945f0614b0a471
griebdaniel <griebdaniel94@gmail.com>
2024-10-17 17:51:20 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [...tseslint.configs.recommended, stylistic.configs['recommended-flat'],],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@stylistic': stylistic
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@stylistic/indent': ['error', 2],  
+    },
+  },
+)
